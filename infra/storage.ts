169548d3b21d908b9628667e212cc1175a36796c
showmen-gupta <shwmndsgpt99@gmail.com>
2024-09-13 14:43:30 +0200

    Enabling CORS

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,4 +1,6 @@
-export const bucket = new sst.aws.Bucket("Uploads");
+export const bucket = new sst.aws.Bucket("Uploads", {
+  cors: true,
+});
 export const secret = new sst.Secret("StripeSecretKey");
 
 // Create the DynamoDB table
